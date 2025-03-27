d3a3c706ea4ec371cce538f4451f31f8432fd98b
Barrycxx <chenxin@bu.edu>
2025-03-27 17:30:59 -0400

    check-in: implemented pars e for one-week milestone

--- assigns/interp1/lib/interp1.ml
+++ assigns/interp1/lib/interp1.ml
@@ -1,16 +1,15 @@
 include Utils
 
-(* Implements parse using the provided parser and lexer.
-   This is the only part required for the one-week check-in. *)
-let parse (s : string) : prog option =
-  try Some (Parser.prog Lexer.read (Lexing.from_string s))
-  with _ -> None
+let parse (s : string) : expr option =
+  match Parser.prog Lexer.read (Lexing.from_string s) with
+  | e -> Some e
+  | exception _ -> None
 
-(* The rest of the functions are not needed for the check-in.
-   They are left as stubs so the code compiles. *)
+let subst (_ : value) (_ : string) (_ : expr) : expr =
+  Unit (* TODO *)
 
-let subst (_ : value) (_ : string) (_ : expr) : expr = Unit
+let eval (_ : expr) : (value, error) result =
+  Ok VUnit (* TODO *)
 
-let eval (_ : expr) : (value, error) result = Ok VUnit
-
-let interp (_ : string) : (value, error) result = Ok VUnit
+let interp (_ : string) : (value, error) result =
+  Ok VUnit (* TODO *)
