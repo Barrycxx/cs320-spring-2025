967bc85f5573e2a13b56f83f50ae674357eb2650
Barrycxx <144643321+Barrycxx@users.noreply.github.com>
2025-04-04 10:12:28 -0400

    Update interp1.ml

--- assigns/interp1/lib/interp1.ml
+++ assigns/interp1/lib/interp1.ml
@@ -23,9 +23,12 @@ let rec subst (v : value) (x : string) (e : expr) : expr =
   | Bop (op, e1, e2) -> Bop (op, subst v x e1, subst v x e2)
   | If (e1, e2, e3) -> If (subst v x e1, subst v x e2, subst v x e3)
   | Let (y, e1, e2) ->
-      if x = y then Let (y, subst v x e1, e2)
-      else Let (y, subst v x e1, subst v x e2)
-  | Fun (y, e1) -> if x = y then Fun (y, e1) else Fun (y, subst v x e1)
+      let e1' = subst v x e1 in
+      if x = y then Let (y, e1', e2)
+      else Let (y, e1', subst v x e2)
+  | Fun (y, e1) ->
+      if x = y then Fun (y, e1)
+      else Fun (y, subst v x e1)
 
 let rec eval (e : expr) : (value, error) result =
   match e with
@@ -45,28 +48,28 @@ let rec eval (e : expr) : (value, error) result =
        | Error err -> Error err)
   | Bop (op, e1, e2) ->
       (match eval e1 with
+       | Error err -> Error err
        | Ok v1 ->
-           (match eval e2 with
-            | Ok v2 ->
-                (match (op, v1, v2) with
-                 | (Add, VNum n1, VNum n2) -> Ok (VNum (n1 + n2))
-                 | (Sub, VNum n1, VNum n2) -> Ok (VNum (n1 - n2))
-                 | (Mul, VNum n1, VNum n2) -> Ok (VNum (n1 * n2))
-                 | (Div, VNum _, VNum 0) -> Error DivByZero
-                 | (Div, VNum n1, VNum n2) -> Ok (VNum (n1 / n2))
-                 | (Mod, VNum _, VNum 0) -> Error DivByZero
-                 | (Mod, VNum n1, VNum n2) -> Ok (VNum (n1 mod n2))
-                 | (Lt, VNum n1, VNum n2) -> Ok (VBool (n1 < n2))
-                 | (Lte, VNum n1, VNum n2) -> Ok (VBool (n1 <= n2))
-                 | (Gt, VNum n1, VNum n2) -> Ok (VBool (n1 > n2))
-                 | (Gte, VNum n1, VNum n2) -> Ok (VBool (n1 >= n2))
-                 | (Eq, VNum n1, VNum n2) -> Ok (VBool (n1 = n2))
-                 | (Neq, VNum n1, VNum n2) -> Ok (VBool (n1 <> n2))
-                 | (And, VBool b1, VBool b2) -> Ok (VBool (b1 && b2))
-                 | (Or, VBool b1, VBool b2) -> Ok (VBool (b1 || b2))
-                 | _ -> Error (InvalidArgs op))
-            | Error err -> Error err)
-       | Error err -> Error err)
+         (match eval e2 with
+          | Error err -> Error err
+          | Ok v2 ->
+            match (op, v1, v2) with
+            | (Add, VNum n1, VNum n2) -> Ok (VNum (n1 + n2))
+            | (Sub, VNum n1, VNum n2) -> Ok (VNum (n1 - n2))
+            | (Mul, VNum n1, VNum n2) -> Ok (VNum (n1 * n2))
+            | (Div, VNum _, VNum 0) -> Error DivByZero
+            | (Div, VNum n1, VNum n2) -> Ok (VNum (n1 / n2))
+            | (Mod, VNum _, VNum 0) -> Error DivByZero
+            | (Mod, VNum n1, VNum n2) -> Ok (VNum (n1 mod n2))
+            | (Lt, VNum n1, VNum n2) -> Ok (VBool (n1 < n2))
+            | (Lte, VNum n1, VNum n2) -> Ok (VBool (n1 <= n2))
+            | (Gt, VNum n1, VNum n2) -> Ok (VBool (n1 > n2))
+            | (Gte, VNum n1, VNum n2) -> Ok (VBool (n1 >= n2))
+            | (Eq, VNum n1, VNum n2) -> Ok (VBool (n1 = n2))
+            | (Neq, VNum n1, VNum n2) -> Ok (VBool (n1 <> n2))
+            | (And, VBool b1, VBool b2) -> Ok (VBool (b1 && b2))
+            | (Or, VBool b1, VBool b2) -> Ok (VBool (b1 || b2))
+            | _ -> Error (InvalidArgs op)))
   | If (e1, e2, e3) ->
       (match eval e1 with
        | Ok (VBool true) -> eval e2
@@ -74,9 +77,12 @@ let rec eval (e : expr) : (value, error) result =
        | Ok _ -> Error InvalidIfCond
        | Error err -> Error err)
   | Let (x, e1, e2) ->
-      (* extra credit: support recursion by replacing x in e1 with itself *)
-      let e1' = subst (VFun (x, e1)) x e1 in
-      match eval e1' with
+      let e1_eval =
+        match e1 with
+        | Fun _ -> subst (VFun (x, e1)) x e1 (* extra credit *)
+        | _ -> e1
+      in
+      match eval e1_eval with
       | Ok v1 -> eval (subst v1 x e2)
       | Error err -> Error err
 
